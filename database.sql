697ff6c553a3f7312d315646bbf809d5a89b63e7
cnlab <cnlab@cnlab11.login.secureunibox.com>
2018-07-11 14:29:13 +0530

    Added database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.16  Distrib 10.1.21-MariaDB, for Win32 (AMD64)
+-- MySQL dump 10.14  Distrib 5.5.43-MariaDB, for Linux (x86_64)
 --
--- Host: localhost    Database: localhost
+-- Host: localhost    Database: users
 -- ------------------------------------------------------
--- Server version	10.1.21-MariaDB
+-- Server version	5.5.43-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,12 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `users`
+-- Table structure for table `itemcategory`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `users` /*!40100 DEFAULT CHARACTER SET latin1 */;
+DROP TABLE IF EXISTS `itemcategory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `itemcategory` (
+  `cat_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`cat_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-USE `users`;
+--
+-- Dumping data for table `itemcategory`
+--
+
+LOCK TABLES `itemcategory` WRITE;
+/*!40000 ALTER TABLE `itemcategory` DISABLE KEYS */;
+INSERT INTO `itemcategory` VALUES ('Clothing'),('Electronics'),('Food');
+/*!40000 ALTER TABLE `itemcategory` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `itemlist`
@@ -31,12 +46,15 @@ DROP TABLE IF EXISTS `itemlist`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `itemlist` (
-  `item_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
   `item_name` varchar(255) DEFAULT NULL,
   `item_category` varchar(255) DEFAULT NULL,
   `item_price` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `item_stock` int(11) DEFAULT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `item_category` (`item_category`),
+  CONSTRAINT `itemlist_ibfk_1` FOREIGN KEY (`item_category`) REFERENCES `itemcategory` (`cat_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +63,7 @@ CREATE TABLE `itemlist` (
 
 LOCK TABLES `itemlist` WRITE;
 /*!40000 ALTER TABLE `itemlist` DISABLE KEYS */;
-INSERT INTO `itemlist` VALUES (1,'Banana','Fruit',100),(2,'Banana Shake','Juices',100);
+INSERT INTO `itemlist` VALUES (1,'Banana','Food',100,10),(2,'Toffee','Food',1,10),(3,'Balaji - Magic Masala','Food',10,10),(4,'T-Shirt','Clothing',500,10),(5,'TV-Remote','Electronics',1000,10),(6,'Apple','Food',100,12),(7,'Orange','Food',100,12),(8,'Levi`s Shirt','Clothing',1000000,1);
 /*!40000 ALTER TABLE `itemlist` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +75,7 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL DEFAULT '0',
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `priv` int(11) DEFAULT NULL,
@@ -71,7 +89,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'dakshpokar','password',3);
+INSERT INTO `users` VALUES (1,'dakshpokar','password',3),(2,'anikotalwar','password',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -84,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-03 17:56:31
+-- Dump completed on 2018-07-11 14:27:04
