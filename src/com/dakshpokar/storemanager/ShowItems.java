6a272d92e383de712a83066c4c8336ed4e6be9ee
Daksh Pokar <dakshpokar@gmail.com>
2018-07-13 21:38:33 +0530

    Added Remove Functionality to ShowItems

--- src/com/dakshpokar/storemanager/ShowItems.java
+++ src/com/dakshpokar/storemanager/ShowItems.java
@@ -11,7 +11,9 @@ import java.awt.Component;
 import java.sql.*;
 
 import javax.swing.border.BevelBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import java.awt.Font;
@@ -23,6 +25,9 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ShowItems {
 
@@ -33,6 +38,7 @@ public class ShowItems {
 	private JLabel label_1;
 	Statement stmt;
 	Connection conn;
+	AddItems addItemsForm = null;
 	/**
 	 * Launch the application.
 	 */
@@ -54,7 +60,7 @@ public class ShowItems {
 	 */
 	private void initialize() throws SQLException, ClassNotFoundException{
 		frame = new JFrame();
-		frame.setBounds(100, 100, 650, 391);
+		frame.setBounds(100, 100, 707, 391);
 		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		String sql;
@@ -76,7 +82,7 @@ public class ShowItems {
 		comboBox.setFont(new Font("Dialog", Font.PLAIN, 18));
 		comboBox.setEditable(true);
 		comboBox.setBackground(Color.WHITE);
-		comboBox.setBounds(422, 48, 140, 40);
+		comboBox.setBounds(370, 35, 140, 40);
 		comboBox.addItem("All");
 		String catsql = "select * from itemcategory";
 		rs3 = stmt.executeQuery(catsql);
@@ -117,14 +123,14 @@ public class ShowItems {
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setBounds(73, 127, 480, 189);
 		JScrollPane scroll=new JScrollPane(table);
-		scroll.setLocation(83, 125);
+		scroll.setLocation(31, 112);
 		scroll.setSize(480, 212);
 		scroll.setViewportBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		
 		frame.getContentPane().add(scroll);
 		
 		JLabel label = new JLabel("List of items found: ");
-		label.setBounds(83, 100, 188, 14);
+		label.setBounds(31, 87, 188, 14);
 		frame.getContentPane().add(label);
 		
 		textField = new JTextField();
@@ -158,12 +164,58 @@ public class ShowItems {
 			}
 		});
 		textField.setFont(new Font("Dialog", Font.PLAIN, 18));
-		textField.setBounds(83, 48, 340, 40);
+		textField.setBounds(31, 35, 340, 40);
 		frame.getContentPane().add(textField);
 		
 		
 		label_1 = new JLabel("Enter the item to be searched:");
-		label_1.setBounds(83, 24, 188, 14);
+		label_1.setBounds(31, 11, 188, 14);
 		frame.getContentPane().add(label_1);
+		
+		JButton button = new JButton("Remove Item");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int rowID = table.getSelectedRow();
+				System.out.println(rowID);
+				if(rowID == -1)
+				{
+					JOptionPane.showMessageDialog(null, "Please select item!");
+				}
+				else {
+					DefaultTableModel model = (DefaultTableModel) table.getModel();
+					int a = Integer.parseInt(table.getValueAt(rowID, 0).toString());
+					String query = "delete from itemlist where item_id = " + a;
+					try {
+						stmt.execute(query);
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					model.removeRow(rowID);
+					table.setModel(model);
+					
+				}
+			}
+		});
+		button.setBounds(531, 67, 137, 25);
+		frame.getContentPane().add(button);
+		
+		JButton button_1 = new JButton("Add Item");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					addItemsForm = new AddItems();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				addItemsForm.frame.setVisible(true);
+			}
+		});
+		button_1.setBounds(531, 35, 137, 25);
+		frame.getContentPane().add(button_1);
 	}
 }
