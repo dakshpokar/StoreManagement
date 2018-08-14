c3f9f6495d018ce9db5153434909b7c8733bf25b
cnlab <cnlab@cnlab11.login.secureunibox.com>
2018-08-14 11:44:47 +0530

    Added Show Bills

--- src/com/dakshpokar/storemanager/ShowBills.java
+++ src/com/dakshpokar/storemanager/ShowBills.java
@@ -4,11 +4,15 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 import java.awt.Component;
 import java.awt.BorderLayout;
+
 import javax.swing.border.BevelBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
 import java.awt.Font;
 
 public class ShowBills {
@@ -34,8 +38,17 @@ public class ShowBills {
 		frmBills = new JFrame();
 		frmBills.getContentPane().setLayout(null);
 		frmBills.setBounds(100, 100, 550, 400);
-		
-		JScrollPane scrollPane = new JScrollPane((Component) null);
+		String sql;
+		sql = "select * from bills";
+		JTable table = new JTable(ClientDashboard.clientConnection.builderFromSender((new Query(sql, 0, 1)))){
+			public boolean isCellEditable(int row, int column){
+			    return false;
+			  }
+			public boolean getScrollableTracksViewportWidth() {
+				   return getPreferredSize().width < getParent().getWidth();
+				 }
+		};
+		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setViewportBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		scrollPane.setBounds(33, 139, 480, 189);
 		frmBills.getContentPane().add(scrollPane);
